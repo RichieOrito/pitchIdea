421546554a39ccc68df72d728eec8f37cbf0590c
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-07 14:30:59 +0300

    This commit creates pitchForm, CommentForm and CategoryForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,45 +1,24 @@
-import email
-from turtle import title
-from click import password_option
+from wtforms.validators import Required
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, PasswordField, ValidationError
-from wtforms.validators import Required, Email, Length, EqualTo
-from ..models import User, Subscribe
 
-class LoginForm(FlaskForm):
+class PitchForm(FlaskForm):
+    """
+    Class to create a wtf form for creating a pitch
+    """
+    content = TextAreaField('Post A Pitch')
+    submit = SubmitField('Submit Pitch')
 
-    username = StringField("Username:", validators=[Required()])
-    password = PasswordField("Password:", validators=[Required()])
-    submit = SubmitField("login")
+class CommentForm(FlaskForm):
+    """
+    Class to create a wtf form for creating a pitch
+    """
+    opinion = TextAreaField('Comment')
+    submit = SubmitField('Submit')
 
-class EditPostForm(FlaskForm):
-
-    title = StringField("", validators=[Required()])
-    content = TextAreaField("", validators=[Required()])
-    submit = SubmitField("post")
-
-class SignUpForm(FlaskForm):
-
-    name = StringField("Name:", validators=[Required()])
-    username = StringField("Username:", validators=[Required()])
-    email = StringField("Email:", validators=[Required(),Email()])
-    password = PasswordField('Password', validators = [Required(),EqualTo('password_confirm',message = 'Passwords do not match')])
-    password_confirm = PasswordField('Confirm Password', validators=[Required()])
-    submit = SubmitField("signUp")
-
-def validate_email(self, data_field):
-    if User.query.filter_by(email = data_field.data).first():
-        raise ValidationError("Email account already Taken")
-
-def validate_username(self, data_field):
-    if User.query.filter_by(username = data_field.data).first():
-        raise ValidationError("Username already exists")
-
-class SubscribeForm(FlaskForm):
-
-    email = StringField("Email:", validators=[Required(), Email()])
-    submit = SubmitField("subscribe")
-
-    def validate_email(self, data_field):
-        if Subscribe.query.filter_by(email = data_field.data).first():
-            raise ValidationError("Email already Subscribed")
+class CategoryForm(FlaskForm):
+    """
+    Class to create a wtf form for creating a pitch
+    """
+    name =  StringField('Category Name', validators=[Required()])
+    submit = SubmitField('Create')
