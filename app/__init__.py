494a230d9bef0d68c6d5cc5253ee2695a71eaa6f
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-07 15:57:57 +0300

    This commits creates app configurations, initializes flask extensions,registers the blueprint and setting of config

--- app/__init__.py
+++ app/__init__.py
@@ -3,46 +3,38 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
-from flask_mail import Mail
-from flask_simplemde import SimpleMDE
-from flask_wtf.csrf import CSRFProtect
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'main.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos',IMAGES)
-mail = Mail()
-csrf = CSRFProtect()
+login_manager = LoginManager()
+
 
 
 def create_app(config_name):
 
     app = Flask(__name__)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app)
+    # Creating the app configurations.
 
-    # Initializing flask extensions
+    app.config.from_object(config_options[config_name])
+
+    # Intitializing flask extensions
+
     bootstrap.init_app(app)
     db.init_app(app)
-    mail.init_app(app)
-    csrf.init_app(app)
     login_manager.init_app(app)
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'auth.login'
+    
+    # Registering the blueprint
 
-        # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
-    # configure UploadSet
-    configure_uploads(app,photos)
+    # setting config
 
     return app
-    
