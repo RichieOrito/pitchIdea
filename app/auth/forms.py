00bc0cb1a94064b02a4224697b41b0481cd5dc60
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-07 14:55:07 +0300

    This commit creates the custom validators class Registration form and a login class

---
+++ app/auth/forms.py
@@ -0,0 +1,38 @@
+from wtforms.validators import Required, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
+from flask_wtf import FlaskForm
+from ..models import User
+
+
+class RegistrationForm(FlaskForm):
+    """
+    RegstrationForm class that passes in the required details for validation
+    """
+
+    email = StringField('your email address', validators=[Required(), Email()])
+    username = StringField('your username', validators=[Required()])
+    password = PasswordField('password', validators=[Required(), EqualTo('password',message='passwords must match')])
+    password_confirm = PasswordField('confirm password', validators=[Required()])
+    submit = SubmitField('sign Up')
+
+    def validate_email(self, data_field):
+        """
+        Functions takes in the data field and checks our database to confirm user Validation
+        """
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+
+    def validate_username(self, data_field):
+        """
+        Function checks if the username is unique and raises ValidationError
+        """
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That user name is already taken. Try another one')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your email address', validators=[Required(),Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
