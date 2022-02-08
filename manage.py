f455d21616aa0c860dd20bdcc977c41e8ce0eb82
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-08 10:20:06 +0300

    changes the production to development

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
 migrate = Migrate(app, db)
